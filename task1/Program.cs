396441585321996988e3a2bd65533effea2f42fa
Igor Akilov <akilovim@gmail.com>
2023-08-29 09:51:24 +0700

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int reverse = 0;
+ReverseNumber(number, reverse);
+
+void ReverseNumber(int num, int rev)
+{
+
+    if(num > 9999 && num < 100000)
+    {
+
+        
+        while (num > 0)
+        {
+            rev *= 10;
+            rev =rev + num % 10;
+            num = num / 10;
+                            
+                
+            
+        }
+        if(rev == number){
+            Console.WriteLine(rev + " Палиндром");
+        }
+        else{
+            Console.WriteLine(rev + " Не палиндром");
+        } 
+           
+
+            
+    }
+    else
+    {
+    Console.WriteLine("Это не пятизначное число.");
+    }
+}
